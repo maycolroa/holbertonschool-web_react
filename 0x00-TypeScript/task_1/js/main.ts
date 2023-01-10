8f2c3a9f64ffac0256e2db52726187fab5baf0c6
maycolroa <lehallo@hotmail.com>
2023-01-10 14:45:18 -0500

    0x00. Typescript 09/01/23 0.05

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -7,3 +7,36 @@ interface Teacher {
     [propName: string]: any;
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+}
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
