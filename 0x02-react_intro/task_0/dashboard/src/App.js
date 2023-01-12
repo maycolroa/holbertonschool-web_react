20fe417189a5a6a5ab97afbe74d9fb10ffa976a5
maycolroa <lehallo@hotmail.com>
2023-01-11 23:14:02 -0500

    0x02. React intro 11/01/23 0.04

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Body from './components/App-body.jsx';
+import Footer from './components/App-footer.jsx';
 import Head from "./components/App-header.jsx";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Head />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ 0x02-react_intro/task_0/dashboard/src/components/App-footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style sheets/App-footer.css"
+
+function Footer () {
+    return (
+        <div className="contenedor-footer">
+            <p className="parrafo-footer-holberton">Copyright 2020 - holberton School</p>
+        </div>
+    )
+}
+
+export default Footer
