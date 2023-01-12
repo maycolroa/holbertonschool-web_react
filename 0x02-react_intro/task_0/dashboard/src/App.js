86fb2f9f9ecc60ab82ee5ff7daa05fd464e59032
maycolroa <lehallo@hotmail.com>
2023-01-11 19:06:18 -0500

    0x02. React intro 11/01/23 0.03

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Body from './components/App-body.jsx';
 import Head from "./components/App-header.jsx";
 
 function App() {
   return (
     <div className="App">
       <Head />
+      <Body />
     </div>
   );
 }
--- 0x02-react_intro/task_1/dashboard/src/App.js
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -4,21 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <img src={logo} alt="hbtn-logo" width="200" /><h1>School dashboard</h1>
     </div>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+    <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+  </div>
   );
 }
 
