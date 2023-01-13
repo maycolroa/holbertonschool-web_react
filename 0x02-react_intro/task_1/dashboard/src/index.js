29d374435d3c53a73180d616ea671a8220a6510f
maycolroa <lehallo@hotmail.com>
2023-01-12 22:22:49 -0500

    0x02. React intro 12/01/23 0.08

--- 0x02-react_intro/task_1/dashboard/src/index.js
+++ 0x02-react_intro/task_1/dashboard/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Notifications from './Notifications';
+import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <div className='root-notifications'>
+      <Notifications />
+      </div>
   </React.StrictMode>
 );
 
+reportWebVitals();
