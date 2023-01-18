abb16ead32226992e8f6918794c2fa95e84868ec
maycolroa <lehallo@hotmail.com>
2023-01-18 15:17:03 -0500

    0x01. Webpack 18/01/23 0.11

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -2,23 +2,17 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
 let count = 0;
 
-function updateCounter() {
-count++;
-$("#count").text(`${count} clicks on the button`);
+function updateCounter () {
+  count++;
+  $('#count').html(`${count} clicks on the button`);
 }
 
-const $button = $("<button>Click here to get started</button>").on(
-"click",
-_.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-})
-);
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+$('button').on('click', _.debounce(updateCounter, 500));
--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,34 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-mode: "production",
-entry: "./js/dashboard_main.js",
-performance: {
-    maxAssetSize: 1000000,
-},
+mode: 'production',
+entry: './js/dashboard_main.js',
 output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-},
-module: {
-    rules: [
-    {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-    },
-    {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-        "file-loader",
-        {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-        },
-        ],
-    },
-    ],
-},
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+}
 };
