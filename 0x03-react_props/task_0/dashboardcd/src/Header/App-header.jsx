29eaba63ca141db38a8b32aee6b2286a6a1f26ad
maycolroa <lehallo@hotmail.com>
2023-01-22 15:52:29 -0500

    0x03. React props 22/01/23 0.3

---
+++ 0x03-react_props/task_0/dashboardcd/src/Header/App-header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App-header.css";
+
+function Head () {
+    return (
+        <div className="contenedor-head">
+            <img className="imagen-holberton" src={require("../img/holberton-logo.jpg")} alt="logo holberton" />
+            <div className="contenedor-texto">
+                <p className="parrafo-holberton">School dashboard</p>
+            </div>
+        </div>
+    )
+}
+
+export default Head;
